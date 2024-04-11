e4f17c3783de1f8994448e712af2c7b5f5317b8b
marcos aguilera <maroctw@hotmail.com>
2024-04-10 22:54:39 -0300

    quitando api facebook

--- src/facebook.js
+++ src/facebook.js
@@ -2,4 +2,4 @@ import facebookAPI from "facebook-nodejs-business-sdk";
 import { variableEntorno } from "./config/env";
 const api = adsSdk.FacebookAdsApi.init(variableEntorno.ACCESS_TOKEN);
 
-facebookAPI.
+
